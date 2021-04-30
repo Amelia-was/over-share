6ca86b8ab4bf6a5d2e154fc140b835c2e1783863
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-30 09:07:45 -0400

    thought form component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { ADD_FRIEND } from '../utils/mutations';
@@ -74,6 +75,8 @@ const Profile = () => {
           />
         </div>
       </div>
+
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
